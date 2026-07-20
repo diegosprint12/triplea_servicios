2bd659f6ab878cfbf3e0e0cc3139082c99bb28c1
Diego Salazar <diegosprint12@gmail.com>
2026-07-20 08:53:11 -0500

    feat: added SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://tripleaservicios.vercel.app',
   output: 'static',
+  integrations: [sitemap()],
   vite: {
     css: {
       postcss: './postcss.config.js',
